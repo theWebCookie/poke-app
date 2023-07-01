10c64cdd5559a21fa89163907aef9f6d76371bcc
theWebCookie <104172333+theWebCookie@users.noreply.github.com>
2023-07-01 15:40:50 +0200

    update Details.jsx

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,21 +1,5 @@
 import React from 'react'
 
-// weightpok={data.weight} 
-//                 heightpok={data.height} 
-//                 pokstat1={data.stats[0].stat.name}
-//                 pokstat2={data.stats[1].stat.name}
-//                 pokstat3={data.stats[2].stat.name}
-//                 pokstat4={data.stats[3].stat.name}
-//                 pokstat5={data.stats[4].stat.name}
-//                 pokstat6={data.stats[5].stat.name}
-                
-//                 posbs1={data.stats[0].base_stat}
-//                 posbs2={data.stats[1].base_stat}
-//                 posbs3={data.stats[2].base_stat}
-//                 posbs4={data.stats[3].base_stat}
-//                 posbs5={data.stats[4].base_stat}
-//                 posbs6={data.stats[5].base_stat}
-
 const Details = (data) => {
   return (
     <div>
